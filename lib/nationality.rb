dbcf41cdd7ab754acd9abd29dc978ddcccef6a75
Joma Minoza <jmminoza@edukasyon.ph>
2017-05-03 03:53:15 +0800

    Added Select Options Helper and Update Nationality Lists

--- lib/nationality.rb
+++ lib/nationality.rb
@@ -7,4 +7,34 @@ module Nationality
 
   NATIONALITY = YML_NATIONALITY.map { |_, v| v }.flatten.compact.sort
 
+  class Nationality
+
+  	class << self
+
+      def count
+        NATIONALITY.map { |v| v }.flatten.count
+      end
+
+      def default_option(default_option)
+        nationalities = NATIONALITY
+
+        default_nationality = default_option.split(" ").map{ |word| word.include?("and") ? word : word.capitalize }.join(" ")
+
+        if nationalities.include? default_nationality
+          nationalities = nationalities - [default_nationality]
+          nationalities.unshift(default_nationality)
+          return nationalities
+        else
+          raise NationalityNotFound
+        end
+      end
+    end
+
+  end
+
+  class NationalityNotFound < StandardError
+    def initialize(msg="Nationality not found on the list")
+      super
+    end
+  end
 end
--- spec/nationality_spec.rb
+++ spec/nationality_spec.rb
@@ -7,16 +7,37 @@ describe Nationality do
   end
 
   context "::NATIONALITY" do
-    it "should be 201" do
-      Nationality::NATIONALITY.count.should == 201
+    it "should be 196" do
+      Nationality::NATIONALITY.count.should == 196
     end
   end
 
-  context "::REGION_DATA" do
-    let(:countries) { Nationality::YML_NATIONALITY.map { |_, v| v}.flatten.sort }
+  describe "::Nationality Class" do
 
-    it "provinces should be consistent with PROVINCES" do
-      countries.should == Nationality::NATIONALITY
+    describe '#default_option method' do
+      context "When Nationality exists" do
+        it "should return the first option selected" do
+          expect(Nationality::Nationality.default_option('filipino')[0]).to eq("Filipino")
+        end
+      end
+
+      context "When Nationality does not exists" do
+        it "should raise NationalityNotFound error" do
+          expect { Nationality::Nationality.default_option('Jejemon') }.to raise_error( Nationality::NationalityNotFound )
+        end
+      end
+    end
+
+    describe '#count method' do
+      it 'should return total count of Nationalities' do
+        expect(Nationality::Nationality.count).to eq(196)
+      end
+    end
+  end
+
+  describe "::NationalityNotFound Error Class" do
+    it "should raise NationalityNotFound error" do
+      expect { raise Nationality::NationalityNotFound }.to raise_error( Nationality::NationalityNotFound )
     end
   end
 end
